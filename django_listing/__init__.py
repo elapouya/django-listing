2f609328ff3c675ea5e958e2628088da659f2856
Eric Lapouyade <eric.lapouyade@jiliti-group.com>
2024-12-17 18:54:26 +0100

    Set form-field min-width to fit-content

--- django_listing/__init__.py
+++ django_listing/__init__.py
@@ -3,7 +3,7 @@
    isort:skip_file
 """
 
-__version__ = "0.9.16"
+__version__ = "0.9.17"
 __author__ = "Eric Lapouyade"
 __copyright__ = "Copyright 2018, The Django listing Project"
 __credits__ = ["Eric Lapouyade"]
